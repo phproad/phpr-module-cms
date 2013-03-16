079cee098af0c1c9cb5cab1aaecec21184f61349
Sam Georges <sam@daftspunk.com>
2013-03-16 23:53:47 +1100

    Fix tabbing

--- assets/javascript/page.js
+++ assets/javascript/page.js
@@ -3,27 +3,22 @@ var url_modified = false;
 jQuery(document).ready(function($){
 
 	var page_title_field = $('#Cms_Page_name');
-	if (page_title_field.length > 0 && $('#new_flag').length > 0)
-	{
+	if (page_title_field.length > 0 && $('#new_flag').length > 0) {
 		page_title_field.bind('keyup', function() { update_url_title(page_title_field) });		
 		page_title_field.bind('change', function() { update_url_title(page_title_field) });
 		page_title_field.bind('paste', function() { update_url_title(page_title_field) });
 	}
 	
-	if ($('#new_flag').length > 0)
-	{
+	if ($('#new_flag').length > 0) {
 		var url_element = $('#Cms_Page_url');
 		url_element.bind('change', function(){ url_modified = true; });
 	}
 
-	function update_url_title(field_element)
-	{
-		if (!url_modified)
-		{
+	function update_url_title(field_element) {
+		if (!url_modified) {
 			$('#Cms_Page_url').val('/' + convert_text_to_url(field_element.val()));
 			$('#Cms_Page_url').trigger('modified');
 		}
 	}
-
 
 });
